119672e8886b256cc7c5a3be418a72e2d016fcdd
Alexander Usmanov <alex.usm@mail.ru>
2019-05-07 15:45:52 +0200

    Add and use command processor

---
+++ Example.IoC.Shell/CommandProcessor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Example.IoC.Shell
+{
+    public class CommandProcessor
+    {
+        public void Process()
+        {
+            using (StreamReader streamReader = File.OpenText("Users.csv"))
+            {
+                while (true)
+                {
+                    String line = streamReader.ReadLine();
+                    if (String.IsNullOrEmpty(line))
+                    {
+                        break;
+                    }
+
+                    String[] lineParts = line.Split(';');
+                    String userName = lineParts[0];
+                    String userEmail = lineParts[1];
+                    DateTime userBirthday = DateTime.Parse(lineParts[2]);
+                    if ((userBirthday.Month == DateTime.Now.Month)
+                        && (userBirthday.Day == DateTime.Now.Day))
+                    {
+                        Console.WriteLine($"{userName} ({userEmail})");
+                    }
+                }
+            }
+        }
+    }
+}
--- Example.IoC.Shell/Program.cs
+++ Example.IoC.Shell/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 
 namespace Example.IoC.Shell
 {
@@ -7,27 +6,8 @@ namespace Example.IoC.Shell
     {
         static void Main(string[] args)
         {
-            using (StreamReader streamReader = File.OpenText("Users.csv"))
-            {
-                while (true)
-                {
-                    String line = streamReader.ReadLine();
-                    if (String.IsNullOrEmpty(line))
-                    {
-                        break;
-                    }
-
-                    String[] lineParts = line.Split(';');
-                    String userName = lineParts[0];
-                    String userEmail = lineParts[1];
-                    DateTime userBirthday = DateTime.Parse(lineParts[2]);
-                    if ((userBirthday.Month == DateTime.Now.Month)
-                        && (userBirthday.Day == DateTime.Now.Day))
-                    {
-                        Console.WriteLine($"{userName} ({userEmail})");
-                    }
-                }
-            }
+            var commandProcessor = new CommandProcessor();
+            commandProcessor.Process();
 
             Console.ReadLine();
         }
